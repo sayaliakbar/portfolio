0b18e6bf584660f8f755919791450b2300248aa9
Ali Akbar <sayaliakbar@gmail.com>
2025-04-09 20:21:33 +0500

    Remove Auth0ProviderWithNavigate wrapper from main.jsx: simplified the rendering structure by directly including the App component within BrowserRouter, enhancing code clarity.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0ProviderWithNavigate } from "./context/auth0-context";
+
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
